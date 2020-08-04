94300a4e27e31e69aa0e0b80483ab380a97aba17
17-193VaniaC <17-193VaniaC@users.noreply.github.com>
2020-08-04 11:57:20 +0700

    config dan htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/KP_Bank-BJB';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
