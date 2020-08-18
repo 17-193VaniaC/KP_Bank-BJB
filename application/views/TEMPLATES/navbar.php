de82eabc7c88ada91847a670c9a9e7f390205cda
17-193VaniaC <17-193VaniaC@users.noreply.github.com>
2020-08-18 10:33:05 +0700

    navbar dikit

--- application/views/TEMPLATES/navbar.php
+++ application/views/TEMPLATES/navbar.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #204d95; position: fixed; width: 100%; padding-left: 0;">
-    <a class="navbar-brand" href="<?= base_url('dashboard'); ?>">BJB</a>
+    <a class="navbar-brand" href="<?= base_url('dashboard'); ?>" style="margin-left: 20px; color: white; margin-right: 20px;">Bank BJB</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="nav-item dropdown ml-auto">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="margin-right: 100px; text-decoration: none; color: white;">
                 <?= $user['NAMA'] ?>
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown"">
                 <a class="dropdown-item" href="#">Action</a>
                 <a class="dropdown-item" href="#">Another action</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>">Logout</a>
+                <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>"><b>Logout</b></a>
             </div>
         </div>
     </div>
